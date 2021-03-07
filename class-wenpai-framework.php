b9818fceb50ce69e3f3e2ce02a47a198b2eb120d
sunxiyuan <sxy@ibadboy.net>
2021-03-07 23:28:05 +0800

    特性：项目初始化

---
+++ class-wenpai-framework.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 文派开发框架
+ *
+ * 轻量优雅的WordPress开发框架
+ *
+ * @version 1.0.0
+ *
+ * @package Wenpai\Wenpai_Framework
+ */
+
+namespace Wenpai\Wenpai_Framework;
+
+/**
+ * 文派开发框架主类
+ */
+class Wenpai_Framework {
+
+}
