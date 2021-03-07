d7c5f1c16c2f993012d6b421290b86d7e1574383
sunxiyuan <sxy@ibadboy.net>
2021-03-08 04:03:57 +0800

    特性：提供创建设置页的API

--- class-framework.php
+++ class-framework.php
@@ -15,11 +15,52 @@ namespace Wenpai;
  * 文派开发框架主类
  */
 if ( !class_exists( 'Framework' ) ) {
+
     class Framework {
 
-        public function __construct() {
+        private static $args = array(
+            'admin_options' => array(),
+        );
 
+        public static function init() {
+            add_action( 'admin_menu', array( __CLASS__, '_create_admin_menu' ) );
+        }
+
+        /**
+         * 创建后台管理员菜单
+         */
+        public static function _create_admin_menu() {
+            foreach ( self::$args['admin_options'] as $item ) {
+                add_submenu_page(
+                    'options-general.php',
+                    $item['menu_title'],
+                    $item['menu_title'],
+                    'manage_options',
+                    $item['menu_slug'],
+                    function () {
+                        echo 'test';
+                    }
+                );
+            }
+        }
+
+        /**
+         * 创建设置页
+         *
+         * @since 1.0.0
+         *
+         * @param string $prefix
+         * @param array $args {
+         *     @type string $menu_title 菜单标题
+         *     @type string $menu_slug  菜单Slug
+         * }
+         */
+        public static function create_options( $prefix, $args ) {
+            self::$args['admin_options'][$prefix] = $args;
         }
 
     }
+
+    Framework::init();
+
 }
