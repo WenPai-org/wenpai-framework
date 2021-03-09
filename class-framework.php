57b0972eb7fd842a53531a95d74faf6c99b1bd6f
sunxiyuan <sxy@ibadboy.net>
2021-03-09 15:33:39 +0800

    特性：引入依赖的静态资源

--- class-framework.php
+++ class-framework.php
@@ -37,6 +37,7 @@ if ( !class_exists( 'Framework' ) ) {
          */
         public static function init() {
             add_action( 'admin_menu', array( __CLASS__, '_create_admin_menu' ) );
+            add_action( 'admin_enqueue_scripts', array( __CLASS__, '_admin_enqueue_scripts' ) );
         }
 
         /**
@@ -61,6 +62,19 @@ if ( !class_exists( 'Framework' ) ) {
                     }
                 );
             }
+        }
+
+        /**
+         * 向WordPress管理后台注入框架依赖的CSS与JS
+         */
+        public static function _admin_enqueue_scripts() {
+            wp_enqueue_style( 'wp-color-picker' );
+
+            wp_enqueue_media();
+            wp_enqueue_script( 'wp-color-picker' );
+            wp_enqueue_script( 'jquery' );
+
+            add_action('admin_footer', [__CLASS__, '_script']);
         }
 
         /**
@@ -124,6 +138,86 @@ if ( !class_exists( 'Framework' ) ) {
          */
         public static function create_section( $prefix, $args ) {
             self::$args['sections'][$prefix] = array_merge( (array)self::$args['sections'][$prefix], $args );
+        }
+
+        /**
+         * @return string
+         */
+        public static function _script() {
+            echo <<<EOT
+              <script>
+              jQuery(document).ready(function($) {
+                $('.wp-color-picker-field').wpColorPicker();
+                $('.group').hide();
+                var activetab = '';
+                if (typeof(localStorage) != 'undefined' ) {
+                  activetab = localStorage.getItem("activetab");
+                }
+
+                if(window.location.hash){
+                  activetab = window.location.hash;
+                  if (typeof(localStorage) != 'undefined' ) {
+                    localStorage.setItem("activetab", activetab);
+                  }
+                }
+
+                if ('' !== activetab && $(activetab).length ) {
+                  $(activetab).fadeIn();
+                } else {
+                  $('.group:first').fadeIn();
+                }
+                $('.group .collapsed').each(function(){
+                  $(this).find('input:checked').parent().parent().parent().nextAll().each(
+                      function(){
+                        if ($(this).hasClass('last')) {
+                          $(this).removeClass('hidden');
+                          return false;
+                        }
+                        $(this).filter('.hidden').removeClass('hidden');
+                      });
+                });
+
+                if ('' !== activetab && $(activetab + '-tab').length ) {
+                  $(activetab + '-tab').addClass('nav-tab-active');
+                }
+                else {
+                  $('.nav-tab-wrapper a:first').addClass('nav-tab-active');
+                }
+                $('.nav-tab-wrapper a').click(function(evt) {
+                  $('.nav-tab-wrapper a').removeClass('nav-tab-active');
+                  $(this).addClass('nav-tab-active').blur();
+                  var clicked_group = $(this).attr('href');
+                  if (typeof(localStorage) != 'undefined' ) {
+                    localStorage.setItem("activetab", $(this).attr('href'));
+                  }
+                  $('.group').hide();
+                  $(clicked_group).fadeIn();
+                  evt.preventDefault();
+                });
+
+                $('.wpsa-browse').on('click', function (event) {
+                  event.preventDefault();
+
+                  var self = $(this);
+
+                  var file_frame = wp.media.frames.file_frame = wp.media({
+                    title: self.data('uploader_title'),
+                    button: {
+                      text: self.data('uploader_button_text'),
+                    },
+                    multiple: false
+                  });
+
+                  file_frame.on('select', function () {
+                    attachment = file_frame.state().get('selection').first().toJSON();
+                    self.prev('.wpsa-url').val(attachment.url).change();
+                  });
+
+                  file_frame.open();
+                });
+              });
+              </script>
+EOT;
         }
 
     }
